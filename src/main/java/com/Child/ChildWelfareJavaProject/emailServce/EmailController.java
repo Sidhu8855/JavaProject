bd090721a0daf85528004546dd8292ed78762b4c
Siddesh8855 <143347761+Sidhu8855@users.noreply.github.com>
2024-05-08 19:09:09 +0530

    Update EmailController.java

--- src/main/java/com/Child/ChildWelfareJavaProject/emailServce/EmailController.java
+++ src/main/java/com/Child/ChildWelfareJavaProject/emailServce/EmailController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 //@RestController
 //	@RequestMapping("/email")
@@ -31,7 +32,7 @@ import org.springframework.web.bind.annotation.*;
 //	    }
 //	}
 
-
+@CrossOrigin()
 @RestController
 @RequestMapping("/email")
 public class EmailController {
